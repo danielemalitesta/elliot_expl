b7f17a089802813be9990b8885b054741f7f759c
daniele <d.malitesta@gmail.com>
2021-03-11 09:55:39 +0100

    Argparse in sample main.

--- sample_main.py
+++ sample_main.py
@@ -1,3 +1,9 @@
 from elliot.run import run_experiment
+import argparse
 
-run_experiment("config_files/custom.yml")
+parser = argparse.ArgumentParser(description="Run sample main.")
+parser.add_argument('--type_experiment', type=str, default='baselines')
+parser.add_argument('--dataset', type=str, default='amazon_men')
+args = parser.parse_args()
+
+run_experiment("config_files/{0}_{1}.yml".format(args.type_experiment, args.dataset))
